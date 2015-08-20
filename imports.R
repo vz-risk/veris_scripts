8fedc46690e8e2d3600ce1b150f0ae39c27bfe44
Gabriel Bassett <gabe@infosecanalytics.com>
2015-08-20 16:15:59 -0500

    A list of imports and some config settings that help prepare an environment when working with VERIS data.

---
+++ imports.R
@@ -0,0 +1,24 @@
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(scales))
+suppressPackageStartupMessages(library(gridExtra))
+suppressPackageStartupMessages(library(tidyr))
+suppressPackageStartupMessages(library(binom))
+suppressPackageStartupMessages(library(knitr))
+suppressPackageStartupMessages(library(verisr))
+suppressPackageStartupMessages(library(pbapply))
+suppressPackageStartupMessages(library(rjson))
+suppressPackageStartupMessages(library(reshape2))
+suppressPackageStartupMessages(library(streamgraph))
+source('/Volumes/verizon/Customer and Partner Data/DBIR/dbir2015-support.R')
+opts_chunk$set(echo=FALSE, warning=FALSE, message=FALSE,
+               results="markdown", prompt=FALSE, error=FALSE,
+               fig.width=8, fig.height=5, cache=FALSE)
+
+theme_set(theme_minimal() + 
+            theme(panel.background=element_rect(fill="floralwhite", color="gray75"),
+                  panel.grid.major=element_line(color="gray75", size=0.1),
+                  axis.ticks=element_blank(),
+                  title = element_text(face="italic", size=10)
+            ))
