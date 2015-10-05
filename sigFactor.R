726b08f8310ef9f7408cf1f12e2e26050e05ddd6
Gabriel Bassett <gabriel.bassett@verizon.com>
2015-10-05 13:27:20 -0500

    updated sigFactor to deal with some bugs. No NAs in group_feature.  exclusion of group_feature from feature_chunk

--- sigFactor.R
+++ sigFactor.R
@@ -16,9 +16,10 @@ DESCRIPTION <- "identify the features which make a group withen an enumeration o
 # WARNING: The distribution comparison algorithm assumes a sum of 100% across a vector.  Since we have some groups that don't have
 #          the feature at all, and some that have more than 100% (say when 2 values for action.hacking.variety are chosen) 
 #          the numbers are not completely accurate and should only be used as a guide for exploring the data.
-# WARNING: Due to the vectors not being exactly as expected as described above, some "Inf" and some "NA" will appear in the results.
-#          I'm not yet sure of the best way to interpret them.  I suspect "Inf" equates to "very prevalent in the group" and 
-#          "NA" equates to "does not exist in the group". Either may indicate relevance.
+# WARNING: Due to the vectors not being exactly as expected as described above, some "Inf" and some "NA"/"NaN" will appear in the results.
+#          I'm not yet sure of the best way to interpret them.  
+#          Inf appears to be when the group is completely in the non-group. e.g. group: c(1,0,1,0) non-group: c(1,5,1,0)
+#          "NA"/"NaN" may equate to "does not exist in the group". Either may indicate relevance.
 # NOTE: I'm handling factors but not hierarchicals like the DBIR actually has 
 #       maybe the bayesian stuff in the entropy package can help with this.
 # NOTE: For continuous variables, I could notionally use em2d(a,b) from emdist (earth movers dist package)
@@ -27,6 +28,11 @@ DESCRIPTION <- "identify the features which make a group withen an enumeration o
 # NOTE: This uses a nested 'for' loop.  I'm sorry.
 # NOTE: This takes a second to run.  See note about nested 'for' loops and me being sorry.
 sigFactors <- function(df, group_feature) {
+    require(dplyr)
+    require(tidyr)
+    require(qdapTools)
+    require(reshape2)
+    require(entropy)
 
     # Get the count of records by group_feature to divide by later
     group_feature_sums <- df %>% group_by_(group_feature) %>% summarize(count=n())
@@ -54,20 +60,40 @@ sigFactors <- function(df, group_feature) {
             # convert counts to percentages
             summarize(sum=n()) %>% 
             # create a matrix dataframe that can then have rows summed and compared to all other rows.
-            dcast(list(group_feature, "value"), value.var="sum", fill=0)
+            reshape2::dcast(list(group_feature, "value"), value.var="sum", fill=0)
         # get cluster as row names
-        rownames(chunk_cast) <- chunk_cast[[group_feature]]
+        if (any(is.na(chunk_cast[[group_feature]]))) {
+            stop("The Group Feature cannot have any values of 'NA'")
+        } else {
+          rownames(chunk_cast) <- chunk_cast[[group_feature]]
+        }
+          
+        feature_chunk_cols <- unique(chunk_gather[chunk_gather$enum == feature]$value)
+        feature_chunk_rows <- unique(chunk_gather[[group_feature]])
+        
+        feature_chunk <- matrix(data=0, 
+                          nrow=length(feature_chunk_rows), 
+                          ncol=length(feature_chunk_cols), 
+                          dimnames=list(feature_chunk_rows, feature_chunk_cols))  %>%
+                         as.data.frame()
 
-        for (group in unique(chunk_gather[[group_feature]])) {
+        feature_chunk[rownames(chunk_cast), colnames(feature_chunk)] <- chunk_cast[,setdiff(colnames(chunk_cast), group_feature)]
+        
+        feature_chunk <- as.matrix(feature_chunk)
 
-            results[group, feature] <- 0.5 * chi2.plugin(chunk_cast[group, 
-                                                                    setdiff(colnames(chunk_cast), group_feature)], 
-                                                         colSums(chunk_cast[setdiff(rownames(chunk_cast), group), 
-                                                                            setdiff(colnames(chunk_cast), group_feature)]))
-            #results[group, feature] <- KL.plugin(chunk_cast[group, 
-            #                                                        setdiff(colnames(chunk_cast), group_feature)], 
-            #                                             colSums(chunk_cast[setdiff(rownames(chunk_cast), group), 
-            #                                                                setdiff(colnames(chunk_cast), group_feature)]))
+        if (ncol(feature_chunk) > 2) {
+            for (group in unique(chunk_gather[[group_feature]])) {
+              
+                g <- feature_chunk[as.character(group), ]
+              
+                not_g <- colSums(feature_chunk[setdiff(rownames(feature_chunk), as.character(group)), ])
+     
+                results[as.character(group), feature] <- 0.5 * chi2.plugin(g, not_g)
+                #results[group, feature] <- KL.plugin(feature_chunk[group, 
+                #                                                        setdiff(colnames(feature_chunk), group_feature)], 
+                #                                             colSums(feature_chunk[setdiff(rownames(feature_chunk), group), 
+                #                                                                setdiff(colnames(feature_chunk), group_feature)]))
+            }
         }
     }
 
