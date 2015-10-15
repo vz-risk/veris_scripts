d031bc73e8819b7ca6bd29e7e632fa1067cf9fab
Gabriel Bassett <gabe@infosecanalytics.com>
2015-10-15 12:39:16 -0500

    updated year2year to produce absolute difference (subtract) rather than relative difference (divide) of percentage of records that year with the enumeration

--- year2year.R
+++ year2year.R
@@ -43,20 +43,29 @@ year2year <- function(df, current_year=NULL, last_year=NULL, filter=TRUE) {
 #  }
   # well, since no-one coded plus.dbir_year in the 2014 data, we'll use incident year
   last_df <- df %>% filter(timeline.incident.year == as.character(as.numeric(last_year) - 1))
-  
-  current_df <- current_df %>% select(-timeline.incident.year, -plus.dbir_year)
-  last_df <- last_df %>% select(-timeline.incident.year, -plus.dbir_year)
-  
+
+  if ("plus.dbir_year" %in% colnames(df)) {  
+    current_df <- current_df %>% select(-timeline.incident.year, -plus.dbir_year)
+    last_df <- last_df %>% select(-timeline.incident.year, -plus.dbir_year)
+  } else {
+    current_df <- current_df %>% select(-timeline.incident.year)
+    last_df <- last_df %>% select(-timeline.incident.year)
+    
+  }
+    
   # get this year's breach percents
   current_df_pcts <- colSums(current_df) / nrow(current_df)
   
   # get last year's breach percents
   last_df_pcts <- colSums(last_df) / nrow(last_df)
   
-  df_ratio <- current_df_pcts/last_df_pcts
-  df_ratio[is.nan(df_ratio)] <- 1
+#  df_ratio <- current_df_pcts/last_df_pcts
+#  df_ratio[is.nan(df_ratio)] <- 1
+  # going from ratio abs difference
+  df_ratio <- current_df_pcts - last_df_pcts 
+  
   # Below line might be useful in dealing with infinits
-  #df_ratio[is.infinite(df_ratio)] <- max(df_ratio) * 1.20
+#  df_ratio[is.infinite(df_ratio)] <- max(df_ratio) * 1.20
   
   # Visibly inespect breach_ratio for fun
   # breach_ratio[order(breach_ratio, decreasing=T)]
@@ -65,20 +74,22 @@ year2year <- function(df, current_year=NULL, last_year=NULL, filter=TRUE) {
   if (filter) {  
     # Need to choose outliers
     # The data is long tailed so no mean/SD
-    percentiles <- quantile(df_ratio[!is.infinite(df_ratio)], c(0.05, 0.95))
+#    percentiles <- quantile(df_ratio[!is.infinite(df_ratio)], c(0.05, 0.95))
+    percentiles <- quantile(abs(df_ratio[!is.infinite(df_ratio)]), c(0.95))
     
     # going to ask if bin size decreases to the median
     # If it is, I'm going to assume this is not a normal distribution and the lower
     #   quantile doesn't matter.  If bins increasing to median bin, 
-    r <- hist(df_ratio, plot=FALSE)
-    median_bin <- length(r$breaks[r$breaks < median(df_ratio)]) + 1
-    use_lower_bound <- is.unsorted(rev(r$density[1:2]))
+#    r <- hist(df_ratio, plot=FALSE)
+#    median_bin <- length(r$breaks[r$breaks < median(df_ratio)]) + 1
+#    use_lower_bound <- is.unsorted(rev(r$density[1:2]))
     
-    if (use_lower_bound) {
-      df_ratio <- df_ratio[df_ratio < percentiles[1] | df_ratio > percentiles[2]]
-    } else {
-      df_ratio <- df_ratio[df_ratio > percentiles[2]]
-    }
+#    if (use_lower_bound) {
+#      df_ratio <- df_ratio[df_ratio < percentiles[1] | df_ratio > percentiles[2]]
+#    } else {
+#      df_ratio <- df_ratio[df_ratio > percentiles[2]]
+#    }
+    df_ratio <- df_ratio[df_ratio > percentiles[1]]
 
     # Change to a data frame and add the record counts
     df_ratio <- df_ratio %>% melt() %>% add_rownames(var="feature")
@@ -96,7 +107,7 @@ year2year <- function(df, current_year=NULL, last_year=NULL, filter=TRUE) {
   }
   
   # Order it
-  df_ratio <- df_ratio %>% arrange(desc(Ratio))
+  df_ratio <- df_ratio %>% arrange(desc(abs(Ratio)))
   
   # Return it
   df_ratio
